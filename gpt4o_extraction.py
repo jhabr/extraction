f3f73f9108ec2fe0a950b4f4c85f5ed17b9aa33e
Jaro Habr <jaro@freshbits.ch>
2024-08-12 22:50:35 +0200

    Update gpt4o call

--- gpt4o_extraction.py
+++ gpt4o_extraction.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import time
 
 import openai
 from dotenv import load_dotenv
@@ -22,7 +23,7 @@ class Position(BaseModel):
 class Invoice(BaseModel):
     date: str
     invoice_number: int
-    position: list[Position]
+    positions: list[Position]
     total_price: float
 
 
@@ -41,24 +42,40 @@ def run():
     with open("/Users/jh/Downloads/documents/fielmann.jpg", "rb") as image_file:
         image_data = base64.b64encode(image_file.read()).decode('utf-8')
 
+    start_time = time.time()
+
     response = OpenAI(api_key=os.getenv("OPEN_AI_API_KEY")).chat.completions.create(
         model='gpt-4o-2024-08-06',
         messages=[
-            {"role": "system", "content": "You are an assistant that extracts structured information from images."},
+            {
+                "role": "system",
+                "content": "You are an assistant that extracts structured information from images."
+                           "Use the supplied tools to assist the user."},
             {
                 "role": "user",
-                "content": (
-                    "Analyze this image and provide the details in the structured format defined by the "
-                    "Optician model.\n"
-                    f"Image: {image_data}"  # Embed the image data directly in the message
-                )
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Analyze this image and provide the details in the structured format defined by the "
+                                "Optician model.",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpg;base64,{image_data}"
+                        }
+                    }
+                ]
             },
         ],
         tools=[openai.pydantic_function_tool(Optician)],
+        temperature=0,
     )
 
     # Print the structured output
-    print(response)
+    print(response.choices[0].message.tool_calls[0].function.arguments)
+
+    print(f"OpenAI response time: {time.time() - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
