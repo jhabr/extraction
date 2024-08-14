e837fbd64d31b65498a1719e8d7efb72fb7db6d5
Jaro Habr <jaro@freshbits.ch>
2024-08-14 23:02:32 +0200

    Add export and cost calculation

--- gpt4o/gpt4o_extraction.py
+++ gpt4o/gpt4o_extraction.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import os
 import time
 
@@ -39,7 +40,7 @@ class Optician(BaseModel):
 def run():
     load_dotenv()
 
-    with open("/Users/jh/Downloads/documents/fielmann.jpg", "rb") as image_file:
+    with open("./gpt4o/images/fielmann.jpg", "rb") as image_file:
         image_data = base64.b64encode(image_file.read()).decode('utf-8')
 
     start_time = time.time()
@@ -72,10 +73,26 @@ def run():
         temperature=0,  # highest reproducibility
     )
 
+    print(f"OpenAI response time: {time.time() - start_time:.2f} seconds")
+
     # Print the structured output
     print(response.choices[0].message.tool_calls[0].function.arguments)
 
-    print(f"OpenAI response time: {time.time() - start_time:.2f} seconds")
+    with open("./gpt4o/export/fielmann.json", "w") as json_file:
+        json.dump(json.loads(response.choices[0].message.tool_calls[0].function.arguments), json_file, indent=4)
+
+    prompt_cost = 2.50 / 10e6 * response.usage.prompt_tokens
+    completion_cost = 10 / 10e6 * response.usage.completion_tokens
+
+    with open("./gpt4o/export/fielmann_costs.json", "w") as cost_json:
+        json.dump({
+            "prompt_tokens": response.usage.prompt_tokens,
+            "completion_tokens": response.usage.completion_tokens,
+            "total_tokens": response.usage.total_tokens,
+            "prompt_cost_$": round(prompt_cost, 6),
+            "completion_cost_$": round(completion_cost, 6),
+            "total_cost_$": round(prompt_cost + completion_cost, 6),
+        }, cost_json, indent=4)
 
 
 if __name__ == "__main__":
